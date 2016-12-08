284480e136ec26d7bd3fcc88e72189f507d3d626
Alan Rodriguez <info@alanrodriguez.me>
2016-12-07 21:14:46 -0800

    Added styling and shuffle video on reload

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -26,11 +26,10 @@ class Main extends React.Component<any, any>{
 		let data;				
 		let videoIds = []
 		$.ajax({
-			url: 'https://www.reddit.com/r/videos/.json',
+			url: 'https://www.reddit.com/r/videos/.json?limit=50',
 			dataType: 'json',
 			success: (json) => {
 				data = json.data.children;
-
 				for (let i = 0; i < data.length; i++) {
 
 					let videoId = this.getVideoId(data[i].data.url);
@@ -38,6 +37,7 @@ class Main extends React.Component<any, any>{
 				}
 
 				videoIds = _.shuffle(videoIds);
+				console.log(videoIds);
 
 				transaction(() => {
 					store.redditVideos = videoIds;
@@ -60,7 +60,6 @@ class Main extends React.Component<any, any>{
 			var ampersandPosition = videoId && videoId.includes('&') ? videoId.indexOf('&') : -1;
 			if (ampersandPosition != -1) {
 				let id = videoId.substring(0, ampersandPosition);
-				console.log(url, id);
 				return id;
 			} else { 
 				return videoId 
@@ -80,12 +79,21 @@ class Main extends React.Component<any, any>{
 				store.currentVideoId = store.redditVideos[store.currentIndex];
 			});
 		}
+		console.log(store.currentVideoId);
 	}
 
 	onStateChange(event) {
 		if (event.data == 0) {
 			this.handleVideoChange(true);
 		}
+	}
+
+	renderLeftArrow() {
+		return <svg className="arrow" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="64px" height="64px" viewBox="0 0 32 32"><path d="M9.346,16L21.7,3.646l0.707,0.707L10.76,16l11.646,11.646L21.7,28.354L9.346,16z"></path></svg>
+	}
+
+	renderRightArrow() {
+		return <svg className="arrow" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="64px" height="64px" viewBox="0 0 32 32"><path d="M22.654,16L10.3,28.354l-0.707-0.707L21.24,16L9.593,4.354L10.3,3.646L22.654,16z"></path></svg>
 	}
 
 	render() {
@@ -101,11 +109,11 @@ class Main extends React.Component<any, any>{
 
 		return (
 			<div className="main">
-				<h1>ZoneOut</h1>
+				<h1 className="title">zoneout</h1>
 				<div className="wrapper">
 					<div className="button">
 						{store.currentIndex > 0 ? 
-							<div className="prev" onClick={() => {this.handleVideoChange(false)}}>Previous Video</div> : 
+							<div className="prev" onClick={() => {this.handleVideoChange(false)}}>{this.renderLeftArrow()}</div> : 
 							null}
 					</div>
 					<div className="player">
@@ -118,7 +126,7 @@ class Main extends React.Component<any, any>{
 					</div>
 					<div className="button">
 						{store.currentIndex < store.redditVideos.length - 1 ? 
-							<div className="next" onClick={() => {this.handleVideoChange(true)}}>Next Video</div> : 
+							<div className="next" onClick={() => {this.handleVideoChange(true)}}>{this.renderRightArrow()}</div> : 
 							null}
 					</div>
 				</div>
